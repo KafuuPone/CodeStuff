99ba5b9a2e0040e1674ebb71bca1ba0547cd7568
KafuuPone <howqi102@gmail.com>
2024-03-16 13:36:35 +0800

    minor changes for plotting battery

--- BatteryMonitor/plotBattery.py
+++ BatteryMonitor/plotBattery.py
@@ -2,19 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from datetime import datetime
+import pandas as pd
 
 current_dir = os.getcwd() # current directory
-current_date = input('Year: ') + '-' + f"{int(input('Month: ')):02}" + '-' + f"{int(input('Day: ')):02}"
+current_date = str(datetime.now().date())
+if (input('Plot data for today? (y/n) ')).lower() != 'y':
+    current_date = input('Year: ') + '-' + f"{int(input('Month: ')):02}" + '-' + f"{int(input('Day: ')):02}"
 
 filepath = f'{current_dir}\\BatteryLog\\{current_date}.npy'
 
 if os.path.exists(filepath):
-    data = np.load(filepath)
-
-    plt.scatter(data['time'], data['percent'], marker='o')
-    plt.title(current_date)
-    plt.xlabel('Time (HH:MM)')
-    plt.ylabel('Battery (%)')
+    rawdata = np.load(filepath)
+    data = {'Time': rawdata['time'], 'Battery': rawdata['percent']}
+    df = pd.DataFrame(data)
+    ax = df.plot(x='Time', y='Battery', marker='o', linestyle='-', markersize=3,legend=False)
+    ax.set_xlabel('Time (HH:MM)')
+    ax.set_ylabel('Battery (%)')
+    ax.set_title(current_date)
     plt.show()
 else:
     print('Data unavailable')
